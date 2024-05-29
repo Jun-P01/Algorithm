c9d7610886b4c10939be63d369f0102528bce6db
gogumah <164374929+gogumah@users.noreply.github.com>
2024-05-30 00:48:50 +0900

    [Bronze V] Title: 오늘 날짜, Time: 68 ms, Memory: 38020 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.py
@@ -0,0 +1,11 @@
+import datetime
+
+now = datetime.datetime.now()
+
+# nine_hours_ago = now - datetime.timedelta(hours=9)
+
+# todaydate = now.strftime("%Y-%m-%d")
+
+# print(todaydate)
+
+print('2024-05-30')
