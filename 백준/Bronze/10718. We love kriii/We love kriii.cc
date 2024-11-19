ab3ee307d9b93c491204ad35025d906a2c5d1eb6
gogumah <mobius0328@gmail.com>
2024-11-20 06:52:50 +0900

    [Bronze V] Title: We love kriii, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string> 
+
+using namespace std;
+
+int main() {
+    string str;
+    getline(cin, str);
+    cout << "강한친구 대한육군" << endl;
+    cout << "강한친구 대한육군" << endl;
+    cout << str << endl;
+    
+    return 0;
+}
